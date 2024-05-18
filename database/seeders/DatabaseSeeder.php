b4f5557d874c1cceebd0345daf0f9829811df0b2
Fakhri17 <fakhricoffe@gmail.com>
2024-05-18 20:48:35 +0700

    fix: adding migrations for database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
+            PermissionSeeder::class,
+            RoleSeeder::class,
             UserSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,40 @@ class UserSeeder extends Seeder
         Schema::disableForeignKeyConstraints();
         User::truncate();
         Schema::enableForeignKeyConstraints();
-        User::factory(2)->create();
+       
+        $superAdmin = User::create([
+            'username' => 'superadmin',
+            'name' => 'Super Admin',
+            'email' => 'admin@gmail.com',
+            'phone_number' => '081234567890',
+            'password' => Hash::make('admin123'),
+            'address' => 'Jl. Raya No. 1',
+        ]);
+
+        $superAdmin->assignRole('Super Admin');
+
+        $umkmOwner = User::create([
+            'username' => 'umkmowner',
+            'name' => 'UMKM Owner',
+            'email' => 'umkmowner@gmail.com',
+            'phone_number' => '081234567891',
+            'password' => Hash::make('umkm123'),
+            'address' => 'Jl. Raya No. 2',
+
+        ]); 
+
+        $umkmOwner->assignRole('UMKM Owner');
+
+        $customer = User::create([
+            'username' => 'customer',
+            'name' => 'Customer',
+            'email' => 'customer@gmail.com',
+            'phone_number' => '081234567892',
+            'password' => Hash::make('customer123'),
+            'address' => 'Jl. Raya No. 3',
+        ]);
+
+        $customer->assignRole('Customer');
 
         // User::insert($users);
     }
