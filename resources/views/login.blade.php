9f37b790fbf6e811e5da24282165ba146aa3ec08
Imastara <sazizimastara@gmail.com>
2024-05-20 22:07:42 +0700

    simple login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+</head>
+<body>
+  <div class="container">
+    <form action="post">
+      <div class="form-email">
+        <p>Email</p>
+        <input type="email" placeholder="Masukan alamat email">
+      </div>
+      <div class="form-password">
+        <p>Password</p>
+        <input type="password" placeholder="Masukan kata sandi">
+      </div>
+      <div class="form-button">
+        <button type="submit">Masuk</button>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
