3e0d5ce8ee7ade5d383c9487f276380a7b73787e
Imastara <sazizimastara@gmail.com>
2024-05-23 12:08:29 +0700

    styling register page

--- resources/views/components/registrasi/konsumen.blade.php
+++ resources/views/components/registrasi/konsumen.blade.php
@@ -1,51 +1,58 @@
 <div>
-    <p>Selamat datang, anda mendaftar sebagai Konsumen</p>
+    <p class="text-center">Selamat datang, anda mendaftar sebagai Konsumen</p>
     <form action="{{ route('register-proses') }}" method="post">
         @csrf
         <div class="form-username">
-            <p>Username</p>
-            <input type="username" name="username" placeholder="Masukan alamat username">
+            <p class="form-label">Username</p>
+            <input type="username" name="username" placeholder="Masukan username">
         </div>
         @error('username')
         <small>{{ $message }}</small>
         @enderror
         <div class="form-name">
-            <p>Name</p>
-            <input type="text" name="name" placeholder="Masukan alamat name">
+            <p class="form-label">Name</p>
+            <input type="text" name="name" placeholder="Masukan nama">
         </div>
         @error('name')
         <small>{{ $message }}</small>
         @enderror
         <div class="form-no-telp">
-            <p>Nomor Telepon</p>
-            <input type="number" name="no-telp" placeholder="Masukan alamat no-telp">
+            <p class="form-label">Nomor Telepon</p>
+            <input type="number" name="no-telp" placeholder="Masukan no telephone">
         </div>
         @error('no-telp')
         <small>{{ $message }}</small>
         @enderror
         <div class="form-address">
-            <p>Alamat</p>
-            <input type="text" name="address" placeholder="Masukan alamat address">
+            <p class="form-label">Alamat</p>
+            <input type="text" name="address" placeholder="Masukan alamat ">
         </div>
         @error('address')
         <small>{{ $message }}</small>
         @enderror
         <div class="form-email">
-            <p>Email</p>
+            <p class="form-label">Email</p>
             <input type="email" name="email" placeholder="Masukan alamat email">
         </div>
         @error('email')
         <small>{{ $message }}</small>
         @enderror
-        <div class="form-password">
-            <p>Password</p>
+        {{-- <div class="form-password">
+            <p class="form-label">Password</p>
             <input type="password" name="password" placeholder="Masukan kata sandi">
+        </div> --}}
+        <div class="form-password">
+            <p class="form-label">Password</p>
+            <div class="input-password">
+                <input type="password" name="password" placeholder="Masukan kata sandi" id="password" autocomplete="current-password">
+                <i class="far fa-eye-slash" id="togglePassword" data-target="password"></i>
+            </div>
         </div>
         @error('password')
         <small>{{ $message }}</small>
         @enderror
         <div class="form-button">
-            <button type="submit">Masuk</button>
+            <button type="submit">Daftar</button>
         </div>
     </form>
 </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,36 +5,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrasi</title>
-    <style>
-        .hidden {
-            display: none;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css">
+    <link rel="stylesheet" href="css/auth/register.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
     <script>
-        function toogleForm(type) {
-            document.getElementById('form-konsumen').classList.add('hidden');
-            document.getElementById('form-umkm').classList.add('hidden');
-            if (type == 'konsumen') {
-                document.getElementById('form-konsumen').classList.remove('hidden');
-            } else if (type == 'umkm') {
-                document.getElementById('form-umkm').classList.remove('hidden');
-            }
+    function toggleForm(type) {
+        document.getElementById('form-konsumen').classList.add('hidden');
+        document.getElementById('form-umkm').classList.add('hidden');
+    
+        document.getElementById('konsumen-btn').classList.remove('active');
+        document.getElementById('umkm-btn').classList.remove('active');
+
+        if (type === 'konsumen') {
+            document.getElementById('form-konsumen').classList.remove('hidden');
+            document.getElementById('konsumen-btn').classList.add('active');
+        } else if (type === 'umkm') {
+            document.getElementById('form-umkm').classList.remove('hidden');
+            document.getElementById('umkm-btn').classList.add('active');
         }
+    }
     </script>
 </head>
 
 <body>
     <div class="container">
-        <h1>Daftar</h1>
-        <button type="button" onclick="toogleForm('konsumen')">Konsumen</button>
-        <button type="button" onclick="toogleForm('umkm')">Pelaku UMKM</button>
-        <div id="form-konsumen" class="hidden">
-            <x-registrasi.konsumen />
-        </div>
-        <div id="form-umkm" class="hidden">
-            <x-registrasi.umkm />
+        <div class="row row-register">
+            <div class="col-lg-5 col-md-5 col-sm-0 d-flex justify-content-center register-left">
+                <img src="{{ URL::to('img/registerBizhub.png')}}" alt="register">
+            </div>
+            <div class="col-lg-7 col-md-7 col-sm-12 col-login-right d-flex justify-content-center register-right">
+                <div class="register-header">
+                    <h1 class="text-center m-0">Daftar</h1>
+                    <div class="button-change">
+                        <button type="button" id="konsumen-btn" class="active" onclick="toggleForm('konsumen')">Konsumen</button>
+                        <button type="button" id="umkm-btn" onclick="toggleForm('umkm')">Pelaku UMKM</button>
+                    </div>
+                    <div id="form-konsumen">
+                        <x-registrasi.konsumen />
+                    </div>
+                    <div id="form-umkm" class="hidden">
+                        <x-registrasi.umkm />
+                    </div>
+                    <div class="register-bottom text-center">
+                        <p>Anda sudah punya akun? <span><a href="{{ route('login') }}">Masuk disini</a></span></p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
+    <script src="js/auth/script.js"></script>
 </body>
 
 </html>
