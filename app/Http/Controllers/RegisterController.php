b2ed81895775ccc8372980a55b27bd331a0558d2
Resyorc <blaskishare@gmail.com>
2024-05-26 16:16:50 +0700

    add role

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class RegisterController extends Controller
 {
@@ -12,7 +13,34 @@ class RegisterController extends Controller
     {
         return view('auth.register');
     }
-    public function register_proses(Request $request)
+    public function register_konsumen(Request $request)
+    {
+        // Validate the request data
+        $request->validate([
+            'username' => 'required|unique:users|min:5|max:255',
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|min:11|max:20',
+            'address' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8|max:255',
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'name' => $request->name,
+            'phone_number' => $request->phone_number,
+            'address' => $request->address,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->assignRole('Customer');
+
+        return redirect()->route('login');
+    }
+
+
+    public function register_umkm(Request $request)
     {
         $request->validate([
             'username' => 'required|unique:users|min:5|max:255',
@@ -20,10 +48,10 @@ class RegisterController extends Controller
             'phone_number' => 'required|min:11|max:20',
             'address' => 'required|string|max:255',
             'email' => 'required|email:dns|max:255|unique:users',
-            'password' => 'required|string|min:5|max:255',
+            'password' => 'required|string|min:8|max:255',
         ]);
 
-        User::create([
+        $user = User::create([
             'username' => $request->username,
             'name' => $request->name,
             'phone_number' => $request->phone_number,
@@ -31,6 +59,8 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
+
+        $user->assignRole('UMKM Owner');
 
         return redirect('login');
     }
