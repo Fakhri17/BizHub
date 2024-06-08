c2c7077eea2047040feabd975628ca049b78651f
Resyorc <blaskishare@gmail.com>
2024-06-08 15:38:18 +0700

    fix validation register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -22,19 +22,20 @@ class RegisterController extends Controller
     public function register_konsumen(Request $request)
     {
         // Validate the request data
-        $request->validate([
-            'username' => 'required|unique:users|min:5|max:255',
-            'name' => 'required|string|max:255',
-            'phone_number' => 'required|min:11|max:20',
-            'address' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|string|min:8|max:255',
-        ]);
+        // $request->validate([
+        //     'username' => 'required|unique:users|min:5|max:255',
+        //     'name' => 'required|string|max:255',
+        //     'phone_number' => 'required|min:11|max:20',
+        //     'address' => 'required|string|max:255',
+        //     'email' => 'required|email|max:255|unique:users',
+        //     'password' => 'required|string|min:8|max:255',
+        // ]);
 
         $user = User::create([
             'username' => $request->username,
             'name' => $request->name,
             'phone_number' => $request->phone_number,
+            'avatar_path' => $request->avatar_path,
             'address' => $request->address,
             'email' => $request->email,
             'password' => Hash::make($request->password),
@@ -62,6 +63,7 @@ class RegisterController extends Controller
             'username' => $request->username,
             'name' => $request->name,
             'phone_number' => $request->phone_number,
+            'avatar_path' => $request->avatar_path,
             'address' => $request->address,
             'email' => $request->email,
             'password' => Hash::make($request->password)
