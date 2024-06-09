99eae4fca4e3327ef3c7698a5d1e3406e3ac598e
Fakhri17 <fakhricoffe@gmail.com>
2024-06-09 16:52:35 +0700

    fix: content homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UmkmProduct;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        return view('index');
+        // count user only UMKM OWNER role spatie
+        $umkm_owner = User::role('UMKM Owner')->count();
+        $user_list = User::count();
+        $umkm_product = UmkmProduct::count();
+        $data = [
+            'umkm_owner' => $umkm_owner,
+            'user_list' => $user_list,
+            'umkm_product' => $umkm_product
+        ];
+        return view('index', compact('data'));
     }
 }
