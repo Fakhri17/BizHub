f73bfd143eee65c7389e3583bc101a8bc42891ab
Fakhri17 <fakhricoffe@gmail.com>
2024-12-28 05:49:49 +0700

    feat: create another register dusk

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -12,13 +12,12 @@ use Illuminate\Foundation\Testing\DatabaseTruncation;
 class RegisterTest extends DuskTestCase
 {
 
-    use DatabaseTruncation;
-
-    private function generateRandomNPWP() {
-        $randomDigits = function($length) {
+    private function generateRandomNPWP()
+    {
+        $randomDigits = function ($length) {
             return implode('', array_map(fn() => rand(0, 9), range(1, $length)));
         };
-    
+
         return sprintf(
             '%s.%s.%s.%s-%s.%s',
             $randomDigits(2),
@@ -28,6 +27,37 @@ class RegisterTest extends DuskTestCase
             $randomDigits(3),
             $randomDigits(3)
         );
+    }
+
+    public function testKonsumeWithoutEmail()
+    {
+        $this->browse(function (Browser $browser) {
+            $username = 'konsumenuser' . uniqid();
+            $browser->visit('/register')
+                ->click('@konsumen')
+                ->typeSlowly('username', $username, 50)
+                ->typeSlowly('name', 'Konsumen Name', 50)
+                ->typeSlowly('password', 'password', 50)
+                ->typeSlowly('phone_number', '08970632441', 50)
+                ->typeSlowly('address', 'Konsumen Address', 50)
+                ->press('Daftar');
+        });
+        Sleep::for(2)->seconds();
+    }
+
+    public function testKonsumenWithoutEmailAndPass()
+    {
+        $this->browse(function (Browser $browser) {
+            $username = 'konsumenuser' . uniqid();
+            $browser->visit('/register')
+                ->click('@konsumen')
+                ->typeSlowly('username', $username, 50)
+                ->typeSlowly('name', 'Konsumen Name', 50)
+                ->typeSlowly('phone_number', '08970632441', 50)
+                ->typeSlowly('address', 'Konsumen Address', 50)
+                ->press('Daftar');
+        });
+        Sleep::for(2)->seconds();
     }
 
     public function testKonsumenTabLoadsCorrectly()
@@ -48,6 +78,50 @@ class RegisterTest extends DuskTestCase
         Sleep::for(2)->seconds();
     }
 
+
+
+    public function testUmkmWithoutEmail()
+    {
+        $this->browse(function (Browser $browser) {
+            $username = 'umkmuser' . uniqid();
+            $browser->visit('/register')
+                ->pause(500)
+                ->waitFor('@tab-umkm', 5)
+                ->click('@tab-umkm')
+                ->pause(500)
+                ->screenshot('tab-umkm')
+                ->typeSlowly('username_umkm', $username, 50)
+                ->typeSlowly('name_umkm', 'umkm Name', 50)
+                ->typeSlowly('password_umkm', 'password', 50)
+                ->typeSlowly('phone_number_umkm', '08970632441', 50)
+                ->typeSlowly('address_umkm', 'umkm Address', 50)
+                ->typeSlowly('npwp', $this->generateRandomNPWP(), 50)
+                ->press('Daftar');
+        });
+        Sleep::for(2)->seconds();
+    }
+
+    public function testUmkmWithoutEmailAndPass()
+    {
+        $this->browse(function (Browser $browser) {
+            $username = 'umkmuser' . uniqid();
+            $browser->visit('/register')
+                ->pause(500)
+                ->waitFor('@tab-umkm', 5)
+                ->click('@tab-umkm')
+                ->pause(500)
+                ->screenshot('tab-umkm')
+                ->typeSlowly('username_umkm', $username, 50)
+                ->typeSlowly('name_umkm', 'umkm Name', 50)
+                ->typeSlowly('phone_number_umkm', '08970632441', 50)
+                ->typeSlowly('address_umkm', 'umkm Address', 50)
+                ->typeSlowly('npwp', $this->generateRandomNPWP(), 50)
+                ->press('Daftar');
+        });
+        Sleep::for(2)->seconds();
+    }
+
+    
     public function testUmkmTabLoadsCorrectly()
     {
         $this->browse(function (Browser $browser) {
