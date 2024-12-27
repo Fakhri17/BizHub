d4ba0d1e33dfe46c278d44d46b7bf74cd73fc175
Resyorc <blaskishare@gmail.com>
2024-12-27 20:31:17 +0700

    fix testing

--- tests/Browser/RegisterConsumenTest.php
+++ tests/Browser/RegisterConsumenTest.php
@@ -16,9 +16,9 @@ class RegisterConsumenTest extends DuskTestCase
 
     public function testKonsumenTabLoadsCorrectly()
     {
-       Sleep::for(2)->seconds();
+        Sleep::for(2)->seconds();
         $this->browse(function (Browser $browser) {
-            $username = 'konsumenuser'.uniqid();
+            $username = 'konsumenuser' . uniqid();
             $email = 'konsumen' . uniqid() . '@example.com';
             $browser->visit('/register')
                 ->click('@konsumen')
@@ -34,26 +34,28 @@ class RegisterConsumenTest extends DuskTestCase
         Sleep::for(2)->seconds();
     }
 
-     public function testUmkmTabLoadsCorrectly()
+    public function testUmkmTabLoadsCorrectly()
     {
         Sleep::for(2)->seconds();
         $this->browse(function (Browser $browser) {
-            $username = 'umkmuser'.uniqid();
+            $username = 'umkmuser' . uniqid();
             $email = 'umkm' . uniqid() . '@example.com';
             $browser->visit('/register')
+                ->pause(2000)
+                ->waitFor('@tab-umkm', 5)
                 ->click('@tab-umkm')
-                ->typeSlowly('username', $username, 50)
-                ->typeSlowly('name', 'umkm Name', 50)
-                ->typeSlowly('email', $email, 50)
-                ->typeSlowly('password', 'password', 50)
-                ->typeSlowly('phone_number', '08970632441', 50)
-                ->typeSlowly('address', 'umkm Address', 50)
+                ->pause(5000)
+                ->screenshot('tab-umkm')
+                ->typeSlowly('username_umkm', $username, 50)
+                ->typeSlowly('name_umkm', 'umkm Name', 50)
+                ->typeSlowly('email_umkm', $email, 50)
+                ->typeSlowly('password_umkm', 'password', 50)
+                ->typeSlowly('phone_number_umkm', '08970632441', 50)
+                ->typeSlowly('address_umkm', 'umkm Address', 50)
                 ->typeSlowly('npwp', '12.312.312.3-213.123', 50)
                 ->press('Daftar');
             Sleep::for(2)->seconds();
         });
         Sleep::for(2)->seconds();
     }
-
-
 }
